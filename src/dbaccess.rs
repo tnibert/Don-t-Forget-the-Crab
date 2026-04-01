95c1c836b7dd3225824d4752d321fe15f5357717
Timothy Nibert <docz2a@gmail.com>
2026-04-02 10:56:42 +1100

    remove dotenv library

--- src/dbaccess.rs
+++ src/dbaccess.rs
@@ -3,8 +3,6 @@ pub mod models;
 
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
-use dotenv::dotenv;
-use std::env;
 
 use crate::schema::recipes;
 use crate::schema::ingredients;
@@ -14,13 +12,11 @@ use crate::recipe::Recipe;
 use crate::units::get_unit;
 use crate::ingredient::Ingredient;
 
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
+static DB_FILE: &str = "./dontforgetthecrab.db";
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+pub fn establish_connection() -> SqliteConnection {
+    SqliteConnection::establish(DB_FILE)
+        .expect(&format!("Error connecting to {}", DB_FILE))
 }
 
 // todo: unit test
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
 
 mod units;
 mod recipe;
